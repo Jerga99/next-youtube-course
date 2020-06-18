dd681c441f20485dca519f5c878987e13a86fa3c
Filip Jerga <jerga99@gmail.comm>
2020-06-18 14:25:29 +0200

    User activation, sends a confirmation email

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const User = require('./db/user');
 const PendingUser = require('./db/pending-user');
+const { sendConfirmationEmail } = require('./mailer');
 
 const cors = require('cors');
 const corsOptions = {
@@ -12,16 +14,24 @@ const corsOptions = {
   optionsSuccessStatus: 200
 }
 
+app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
 app.get('/api/test', (req, res) => {
   res.json({message: 'Hello World!'});
 })
 
-app.get('api/activate/user/:hash', (req, res) => {
+app.get('/api/activate/user/:hash', async (req, res) => {
   const { hash } = req.params;
-
-  res.json({message: `User ${hash} has been activated`})
+  try {
+    const user = await PendingUser.find({_id: hash});
+    const newUser = new User({...user.data});
+    await newUser.save();
+    await user.remove();
+    res.json({message: `User ${hash} has been activated`})
+  } catch {
+    res.status(422).send('User cannot be activated!');
+  }
 })
 
 app.delete('/api/user/delete', async (req, res) => {
@@ -35,27 +45,30 @@ app.delete('/api/user/delete', async (req, res) => {
 })
 
 app.post('/api/register', async (req, res) => {
+  const { email, username, password } = req.body;
   try {
-    const rUser = await User.find({email: 'test'});
-    const pUser = await PendingUser.find({email: 'test'});
+    const rUser = await User.find({email});
+    const pUser = await PendingUser.find({email});
     if (pUser || rUser) { return res.status(422).send('User is already registered!');}
 
-    const newUser = new PendingUser({email: 'test', username: 'testtest', password: 'hello'});
+    const newUser = new PendingUser({email, username, password});
     await newUser.hashPassword();
+    await sendConfirmationEmail({toUser: newUser.data, hash: newUser.data._id })
 
     const user = await newUser.save();
-    res.json(user);
+    res.json({message: 'You have been registered.'});
   } catch(e) {
     res.status(422).send(e.message);
   }
 })
 
 app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const user = await User.find({email: 'test'});
+    const user = await User.find({email});
     if (!user) { return res.status(422).send('Invalid credentials!')}
 
-    const isValid = await user.validatePassword('hello');
+    const isValid = await user.validatePassword(password);
     if (!isValid) { return res.status(422).send('Invalid password!')}
 
     res.json({message: 'You have been succesfuly logged in'});
---
+++ server/mailer.js
@@ -0,0 +1,36 @@
+
+const nodemailer = require('nodemailer');
+
+exports.sendConfirmationEmail = function({toUser, hash}) {
+  return new Promise((res, rej) => {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GOOGLE_USER,
+        pass: process.env.GOOGLE_PASSWORD
+      }
+    })
+
+    const message = {
+      from: process.env.GOOGLE_USER,
+      // to: toUser.email // in production uncomment this
+      to: process.env.GOOGLE_USER,
+      subject: 'Your App - Activate Account',
+      html: `
+        <h3> Hello ${toUser.username} </h3>
+        <p>Thank you for registering into our Application. Much Appreciated! Just one last step is laying ahead of you...</p>
+        <p>To activate your account please follow this link: <a target="_" href="${process.env.DOMAIN}/api/activate/user/${hash}">${process.env.DOMAIN}/activate </a></p>
+        <p>Cheers</p>
+        <p>Your Application Team</p>
+      `
+    }
+
+    transporter.sendMail(message, function(err, info) {
+      if (err) {
+        rej(err)
+      } else {
+        res(info)
+      }
+    })
+  })
+}
